88ceaf56340a3645b8eba0098a0f91dfdd61082f
saci zakaria <sacideveloppeur@gmail.com>
2023-04-03 19:00:12 +0200

    MaterialApp Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: ' Flutter Grocery',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+          title: const Text("Grocery Admin Panel"),
+        ),
+        body: const Center(child: Text("Flutter Grocery Admin Panel")),
+      ),
+    );
   }
 }
