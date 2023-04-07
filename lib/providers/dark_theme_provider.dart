f07c79813bc9cc63ed80f208afce464f060e7846
saci zakaria <sacideveloppeur@gmail.com>
2023-04-07 16:54:03 +0200

    DarkThemePreference();

--- lib/providers/dark_theme_provider.dart
+++ lib/providers/dark_theme_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../services/dark_them_preferences.dart';
+
 class DarkThemeProvider with ChangeNotifier {
+  DarkThemePreference darkThemePreference = DarkThemePreference();
+
   bool _darkTheme = false;
   bool get getDarkTheme => _darkTheme;
   set setDarkTheme(bool value) {
