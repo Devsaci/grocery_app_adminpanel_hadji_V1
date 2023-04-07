ea30402a872a0761f8c4a6dbb09a554adb7a1723
saci zakaria <sacideveloppeur@gmail.com>
2023-04-07 11:19:52 +0200

    set setDarkTheme(bool value)

--- lib/providers/dark_theme_provider.dart
+++ lib/providers/dark_theme_provider.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class DarkThemeProvider with ChangeNotifier {
   bool _darkTheme = false;
   bool get getDarkTheme => _darkTheme;
+  set setDarkTheme(bool value) {}
 }
