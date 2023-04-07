105556986120f20d174f21a5dced11ee97d5253f
saci zakaria <sacideveloppeur@gmail.com>
2023-04-07 17:21:59 +0200

    void initState()

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,12 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [],
