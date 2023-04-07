518c4b1333b68511912b3fe35a6aa076eb1d7a77
saci zakaria <sacideveloppeur@gmail.com>
2023-04-07 10:17:26 +0200

    SharedPreferences prefs =

--- lib/services/dark_them_preferences.dart
+++ lib/services/dark_them_preferences.dart
@@ -3,6 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class DarkThemePreference {
   static const THEME_STATUS = "THEMESTATUS";
   setDarkTheme(bool value) async {
-    await SharedPreferences.getInstance();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
   }
 }
