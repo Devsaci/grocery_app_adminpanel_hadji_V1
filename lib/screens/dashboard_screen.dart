19fb6e886f9a80eff12dc7cdefecd2d98489c1dc
saci zakaria <sacideveloppeur@gmail.com>
2023-04-10 20:27:44 +0200

    Row  Column

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/header.dart';
+
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Column(
-        children: [],
+        children: [
+          Header(),
+          Row(
+            children: [
+              Column(
+                children: const [
+                  // MyProductsHome(),
+                  // SizedBox(height: defaultPadding),
+                  // OrdersScreen(),
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
