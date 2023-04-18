d328bb0764a16218a827be96bdc4912e2f8a5f52
saci zakaria <sacideveloppeur@gmail.com>
2023-04-18 22:38:40 +0200

    borderRadius  color

--- lib/widgets/products_widget.dart
+++ lib/widgets/products_widget.dart
@@ -11,6 +11,8 @@ class _ProductWidgetState extends State<ProductWidget> {
   @override
   Widget build(BuildContext context) {
     return Material(
+      borderRadius: BorderRadius.circular(12),
+      color: Theme.of(context).cardColor.withOpacity(0.9),
       child: InkWell(
         onTap: () {},
       ),
