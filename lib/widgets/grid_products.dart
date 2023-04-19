b7d787a518703f243089192115e7025e248d80eb
saci zakaria <sacideveloppeur@gmail.com>
2023-04-19 23:57:08 +0200

    crossAxisCount / childAspectRatio

--- lib/widgets/grid_products.dart
+++ lib/widgets/grid_products.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ProductGridWidget extends StatelessWidget {
-  const ProductGridWidget({super.key});
-
+  const ProductGridWidget({
+    super.key,
+    required this.crossAxisCount,
+    required this.childAspectRatio,
+  });
+  final int crossAxisCount;
+  final double childAspectRatio;
+  // final bool isInMain;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
